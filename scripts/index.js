040813587a360aefabdec7430589c8271cf87a0e
Olya Luksha <lukshaolya98@gmail.com>
2024-11-26 14:44:05 +0100

    create html tree plus add styles

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+const CLIENTS_API = "https://rickandmortyapi.com/api/character";
+const PRODUCTS_API = "https://fakestoreapi.com/products?limit=6";
+const FEEDBACK_API =
+  "https://fakerapi.it/api/v1/texts?_quantity=6&_characters=300";
+
+const mainSectionDiv = document.querySelector(".main-section");
